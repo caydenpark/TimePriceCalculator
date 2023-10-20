d1a6cb4adbd42e25e7c03f277802638473a84917
Cayden <114540789+caydenpark@users.noreply.github.com>
2023-10-19 19:14:54 -0600

    UI Mostly developed

--- TimePriceCalculator/ViewController.swift
+++ TimePriceCalculator/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dollarPrice: UILabel!
+    
+    @IBOutlet weak var hourlyWage: UILabel!
 
+    @IBOutlet weak var timePrice: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateTimePrice(_ sender: Any) {
+        
+    }
 }
 
