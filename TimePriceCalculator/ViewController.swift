4918c22e6a4bd633656c8aca90ae70236de8f996
Cayden <114540789+caydenpark@users.noreply.github.com>
2023-10-26 21:12:13 -0600

    Calulator fully functional

--- TimePriceCalculator/ViewController.swift
+++ TimePriceCalculator/ViewController.swift
@@ -9,19 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var dollarPrice: UILabel!
-    
-    @IBOutlet weak var hourlyWage: UILabel!
-
-    @IBOutlet weak var timePrice: UILabel!
+    // Outlets to connect with interface elements in the storyboard
+    @IBOutlet weak var hourlyWage: UITextField!
+    @IBOutlet weak var dollarPrice: UITextField!
+    @IBOutlet weak var timePrice: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // This function is called when the view loads
+        // Additional setup can be performed here
     }
 
+    // This function is connected to the "Calculate" button
     @IBAction func calculateTimePrice(_ sender: Any) {
-        
+        // Attempt to extract & convert text input to Floats
+        if let hourlyWageText = hourlyWage.text,
+           let dollarPriceText = dollarPrice.text,
+           let hourlyWageValue = Float(hourlyWageText),
+           let dollarPriceValue = Float(dollarPriceText)
+        {
+            // If all conversions are successful, calculate timePriceValue.
+            let timePriceValue = dollarPriceValue / hourlyWageValue
+//            timePrice.text = "\(timePriceValue) hr"
+            timePrice.text = "\(String(format: "%.2f", timePriceValue)) hr"
+        } else {
+            timePrice.text = "Invalid input"
+        }
     }
 }
-
